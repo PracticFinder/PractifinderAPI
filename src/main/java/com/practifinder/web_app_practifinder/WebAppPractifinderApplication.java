ee0b769a83e9904a8ee5bd19d71708e8a24edd27
Rafael Primo <rafape1827@gmail.com>
2023-10-27 13:34:44 -0500

    Version inicial del backend
    Generado por SpringInitializr

---
+++ src/main/java/com/practifinder/web_app_practifinder/WebAppPractifinderApplication.java
@@ -0,0 +1,13 @@
+package com.practifinder.web_app_practifinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppPractifinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppPractifinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practifinder/web_app_practifinder/WebAppPractifinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practifinder.web_app_practifinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppPractifinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
